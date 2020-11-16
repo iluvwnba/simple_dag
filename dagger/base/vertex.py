e292e944c5f4ceb6e766a22cee2f8bdcd885942c
Martin Barnes <Martinp1l1l1l@gmail.com>
2020-11-16 20:29:17 +0000

    Operators and a Bash Operator implemented

--- dagger/base/vertex.py
+++ dagger/base/vertex.py
@@ -1,5 +1,11 @@
 from __future__ import annotations
 import uuid
+import subprocess
+import time
+
+
+class OperatorException(Exception):
+    pass
 
 
 class Vertex:
@@ -20,5 +26,35 @@ class Vertex:
 
 
 class Operator(Vertex):
-    def __init__(self, v_id: str = None):
+    def __init__(self, delay: int = 0, v_id: str = None, p_weight: int = 1):
         super(Operator, self).__init__(v_id=v_id)
+        self._priority_weight: int = p_weight
+        self._delay: int = delay
+    # Execution locker
+    _lock_for_execution = False
+
+    def execute(self):
+        pass
+
+
+class BashOperator(Operator):
+    def __init__(self, cmd: str, delay: int = 0, v_id: str = None, p_weight: int = 1):
+        super(BashOperator, self).__init__(delay, v_id, p_weight)
+        self.cmd: str = cmd
+
+    def is_running(self):
+        return self._lock_for_execution
+
+    def execute(self) -> str:
+        if self.is_running():
+            raise OperatorException()
+        self._lock_for_execution = True
+        # Do stuff faking delay
+        time.sleep(self._delay)
+        out_b = subprocess.check_output([self.cmd])
+        out_t: str = out_b.decode('utf-8')
+
+        # Completed execution for the job
+        self._lock_for_execution = False
+
+        return out_t
